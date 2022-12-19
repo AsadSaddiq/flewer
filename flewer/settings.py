03c1d8337d0521430a6cf2f35e2cf29f746b07f7
captanAsad <asadryk2911@gmail.com>
2022-12-19 09:57:04 +0500

    code

--- flewer/settings.py
+++ flewer/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 import os
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
